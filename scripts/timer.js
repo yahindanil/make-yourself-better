0d6dd7bce527591908563834850814814d6838f9
stuffsel <yahin.dd@ya.ru>
2023-01-08 21:42:10 +0300

    Timer added

---
+++ scripts/timer.js
@@ -0,0 +1,48 @@
+window.onload = function () {
+  const time_el = document.querySelector(".timer");
+
+  const start_btn = document.getElementById("start");
+  const stop_btn = document.getElementById("stop");
+  const reset_btn = document.getElementById("reset");
+
+  let seconds = -1;
+  let interval = null;
+
+  start_btn.addEventListener("click", star);
+  stop_btn.addEventListener("click", stop);
+  reset_btn.addEventListener("click", reset);
+
+  function timer() {
+    seconds++;
+
+    let hrs = Math.floor(seconds / 3600);
+    let mins = Math.floor((seconds - hrs * 3600) / 60);
+    let secs = seconds % 60;
+
+    if (secs < 10) secs = "0" + secs;
+    if (mins < 10) mins = "0" + mins;
+    if (hrs < 10) hrs = "0" + hrs;
+
+    time_el.innerText = `${hrs}:${mins}:${secs}`;
+  }
+  timer();
+
+  function star() {
+    if (interval) {
+      return;
+    }
+
+    interval = setInterval(timer, 1000);
+  }
+
+  function stop() {
+    clearInterval(interval);
+    interval = null;
+  }
+
+  function reset() {
+    stop();
+    seconds = 0;
+    time_el.innerText = "00:00:00";
+  }
+};
