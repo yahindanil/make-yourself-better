a4be55e0f00f28935d4c7fe4c6acaeaf12692199
stuffsel <yahin.dd@ya.ru>
2023-01-08 17:45:24 +0300

    Links added

---
+++ scripts/linkButton.js
@@ -0,0 +1,11 @@
+function openAllLinks() {
+  let locs = [
+    "https://www.youtube.com/watch?v=jfKfPfyJRdk&ab_channel=LofiGirl",
+    "https://learn.javascript.ru/",
+    "https://docs.google.com/document/d/1feWvbbsMW4zLiFgQLqTqNR1UU4IEevh4XN-0fDk1qFY/edit",
+  ];
+
+  for (let i = 0; i < locs.length; i++) {
+    window.open(locs[i]);
+  }
+}
