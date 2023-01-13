023aba4f60074d8b1faf0368666655259af3f975
stuffsel <yahin.dd@ya.ru>
2023-01-13 17:30:59 +0300

    New timer with saving progress

--- scripts/Time functions/Timer/Browser logic/timerBrowser.js
+++ scripts/Time functions/Timer/Browser logic/timerBrowser.js
@@ -3,10 +3,8 @@ import {
   resetTimerBusiness,
   setTimerStartDate,
   saveSecondsOnStop,
-  resetSecondsOnStop,
+  secondsToString,
 } from "../Business logic/timerBusiness.js";
-
-export let time = 0;
 
 const time_display = document.querySelector(".timer");
 
@@ -17,6 +15,14 @@ const timer_reset_btn = document.getElementById("timer-reset-btn");
 timer_start_btn.addEventListener("click", startTimerBrowser);
 timer_stop_btn.addEventListener("click", timerStopBrowser);
 timer_reset_btn.addEventListener("click", timerResetBrowser);
+
+//Меняет значение таймера при запуске сайта
+if (localStorage.getItem("localSecondsOnBrowserRestart") > 0) {
+  time_display.innerText = secondsToString(
+    localStorage.getItem("localSecondsOnBrowserRestart")
+  );
+  console.log("test");
+}
 
 let interval = null;
 let isIntervalRunning = false;
@@ -39,11 +45,13 @@ function changeTime() {
 function timerStopBrowser() {
   clearInterval(interval);
   isIntervalRunning = false;
+
   saveSecondsOnStop();
 }
 
 function timerResetBrowser() {
   timerStopBrowser();
-  time_display.innerText = resetTimerBusiness();
-  resetSecondsOnStop();
+  time_display.innerText = "00:00:00";
+
+  resetTimerBusiness();
 }
--- scripts/Time functions/Timer/Business logic/timerBusiness.js
+++ scripts/Time functions/Timer/Business logic/timerBusiness.js
@@ -1,26 +1,36 @@
-let seconds = 0;
+if (!localStorage.getItem("localSecondsOnBrowserRestart")) {
+  localStorage.setItem("localSecondsOnBrowserRestart", 0);
+}
+
+let secondsOnBrowserRestart = Number(
+  localStorage.getItem("localSecondsOnBrowserRestart")
+);
+
 let timerStartDate;
 let secondsSavedOnStop = 0;
+let seconds = 0;
 
 export function getTime() {
   let currentDate = new Date();
   seconds =
-    secondsSavedOnStop + Math.floor((currentDate - timerStartDate) / 1000);
+    Math.floor((currentDate - timerStartDate) / 1000) + secondsSavedOnStop;
 
+  seconds += secondsOnBrowserRestart;
+
+  localStorage.setItem("localSecondsOnBrowserRestart", seconds);
+
+  return secondsToString(seconds);
+}
+
+export function secondsToString(seconds) {
   let hrs = Math.floor(seconds / 3600);
   let mins = Math.floor((seconds - hrs * 3600) / 60);
   let secs = seconds % 60;
-
   if (secs < 10) secs = "0" + secs;
   if (mins < 10) mins = "0" + mins;
   if (hrs < 10) hrs = "0" + hrs;
 
   return `${hrs}:${mins}:${secs}`;
-}
-
-export function resetTimerBusiness() {
-  seconds = 0;
-  return "00:00:00";
 }
 
 export function setTimerStartDate() {
@@ -31,21 +41,41 @@ export function saveSecondsOnStop() {
   secondsSavedOnStop = seconds;
 }
 
-export function resetSecondsOnStop() {
+export function resetTimerBusiness() {
+  seconds = 0;
   secondsSavedOnStop = 0;
+  localStorage.setItem("localSecondsOnBrowserRestart", 0);
 }
 
-// export function getTime() {
-//   seconds++;
+// let timerStartDate;
+// let secondsSavedOnStop = 0;
+// let seconds = 0;
 
-//   // переписать под Date
+// export function getTime() {
+//   let currentDate = new Date();
+//   seconds =
+//     secondsSavedOnStop + Math.floor((currentDate - timerStartDate) / 1000);
+
+//   //Перевод seconds к формату "00:00:00"
 //   let hrs = Math.floor(seconds / 3600);
 //   let mins = Math.floor((seconds - hrs * 3600) / 60);
 //   let secs = seconds % 60;
-
 //   if (secs < 10) secs = "0" + secs;
 //   if (mins < 10) mins = "0" + mins;
 //   if (hrs < 10) hrs = "0" + hrs;
 
 //   return `${hrs}:${mins}:${secs}`;
+// }
+
+// export function setTimerStartDate() {
+//   timerStartDate = new Date();
+// }
+
+// export function saveSecondsOnStop() {
+//   secondsSavedOnStop = seconds;
+// }
+
+// export function resetTimerBusiness() {
+//   seconds = 0;
+//   secondsSavedOnStop = 0;
 // }
