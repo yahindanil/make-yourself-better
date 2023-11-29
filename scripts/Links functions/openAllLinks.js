90de56ad7d53d54b62edf80de4ff0d890e075d14
yahindanil <ahin.danil@gmail.com>
2023-11-29 14:59:06 +0400

    links add button fix

--- scripts/Links functions/openAllLinks.js
+++ scripts/Links functions/openAllLinks.js
@@ -1,6 +1,8 @@
 const OpenAllLinksBtn = document.getElementById("open-all-links");
 OpenAllLinksBtn.addEventListener("click", openAllLinks);
 
+hideOpenAllLinksBtn();
+
 function openAllLinks() {
   let links;
   if (localStorage.getItem("links") === null) {
@@ -14,8 +16,12 @@ function openAllLinks() {
   }
 }
 
-if (JSON.parse(localStorage.getItem("links")).length >= 2) {
-  OpenAllLinksBtn.classList.remove("hidden");
+function hideOpenAllLinksBtn() {
+  if (localStorage.getItem("links") === null) {
+    return;
+  } else if (JSON.parse(localStorage.getItem("links")).length >= 2) {
+    OpenAllLinksBtn.classList.remove("hidden");
+  }
 }
 
 export function hideRevealOpenAllLinks(element) {
